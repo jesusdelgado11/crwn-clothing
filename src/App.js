07b1a72dc7a3050fdc779a5342e4e04fcb343cea
Jesus <jesusdelgado2@yahoo.com>
2021-06-24 21:30:16 -0500

    broken our homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
 function App() {
-  return (
-    <div>
-      <HomePage />
-    </div>
+  return (    
+    <HomePage />
   );
 }
 
